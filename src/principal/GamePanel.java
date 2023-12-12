5c2dbb03c9f9de9cf6fd344d7c98bddbc405e1f9
Eduardo Rocha Azevedo <142415927+Eduardo-Rocha-Azevedo@users.noreply.github.com>
2023-12-11 21:19:17 -0300

    inicio
    Aquirvos basícos para o desenvolvimento de jogos com a linguagem Java pura

---
+++ src/principal/GamePanel.java
@@ -0,0 +1,93 @@
+package principal;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+import tile.TileManeger;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class GamePanel extends JPanel implements Runnable{
+
+	//screen setings 
+	final int originalTilesSize = 16;// 16px X 16px
+	final int scale = 3;
+	
+	public final int tileSize = originalTilesSize * scale;
+    public final int maxScreenCols = 16;
+    public final int maxScreenRows = 12;
+    public final int screenWith = tileSize * maxScreenCols;//768px
+    public final int screenHeight = tileSize * maxScreenRows;//576px
+
+    //FPS
+    int FPS = 60;
+    TileManeger tileM = new TileManeger(this); 
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;  
+    Player player = new Player(this,keyH);
+
+    //constructor======================
+    public GamePanel() {
+     this.setPreferredSize(new Dimension(screenWith, screenHeight));
+     this.setBackground(new Color(0,0,0));
+     this.setDoubleBuffered(true);
+     this.addKeyListener(keyH);
+     this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        if(gameThread == null){
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+    }
+
+    @Override
+    public void run(){  
+        double drawInterval = 1000000000/FPS;
+        double delta = 0; 
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;  
+                drawCount++;
+            }   
+
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update(){
+      
+    	player.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        tileM.draw(g2);
+
+        player.draw(g2);
+
+        g2.dispose();
+    }
+}
---
+++ src/tile/TileManeger.java
@@ -0,0 +1,93 @@
+package tile;
+
+import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.imageio.ImageIO;
+import principal.GamePanel;
+
+public class TileManeger {
+    GamePanel gp;
+    Tile[] tile;
+    int mapTileNum[][];
+
+    public TileManeger(GamePanel gp){
+        this.gp = gp; 
+        tile = new Tile[10];
+        mapTileNum = new int[gp.maxScreenCols][gp.maxScreenRows];
+        getTileImage();
+        loadMap();
+    }
+
+    public void getTileImage(){
+        try{
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadMap(){
+        try {
+            InputStream is = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while(col < gp.maxScreenCols && row < gp.maxScreenRows){
+              String line = br.readLine();
+
+              while (col < gp.maxScreenCols) {
+                String numbers[] = line.split(" ");
+
+                int num = Integer.parseInt(numbers[col]);
+
+                mapTileNum[col][row] = num;
+                col++;
+              }
+              if(col == gp.maxScreenCols){
+                col = 0;
+                row++;
+              }
+            }
+         br.close();
+
+        } catch (Exception e) {
+          
+        }
+    }
+
+    public void draw(Graphics2D g2){
+       int col = 0;
+       int row = 0;
+       int x = 0;
+       int y = 0;
+       
+       while(col < gp.maxScreenCols && row < gp.maxScreenRows){
+
+            int tileNum = mapTileNum[col][row]; 
+
+            g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+            col++;
+            x += gp.tileSize;
+
+            if(col == gp.maxScreenCols){
+                col = 0;
+                x = 0;
+                row++;
+                y += gp.tileSize; 
+            }
+       }
+    }
+}
